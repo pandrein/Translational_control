760a0fc3e2b7ff78a72a8ee34cd5784c7b4dcb40
Paolo <paolo.andreini@yahoo.it>
2020-07-10 09:54:11 +0200

    add parallel computation and histogram save

--- python/main.py
+++ python/main.py
@@ -1,26 +1,26 @@
+import multiprocessing
+import os
+import time
+from itertools import combinations
+from pathlib import Path
+import matplotlib.pyplot as plt
+import pandas as pd
+from Comparison_digital_profiles_human import compare_pair
+from Signal_digitalisation_human import MatricesExtractor
 from utils import InputFileManager
 from utils import create_dir_if_not_exist
-from Signal_digitalisation_human import MatricesExtractor
-from Comparison_digital_profiles_human import compare_pair
-from itertools import combinations
-import os
-import pandas as pd
-from pathlib import Path
-import sys
-import numpy as np
-import matplotlib.pyplot as plt
-from joblib import Parallel, delayed
-import multiprocessing
-import time
 
-num_cores = multiprocessing.cpu_count()
+# num_cores = multiprocessing.cpu_count()
 
 # I/O directories
 input_dir = os.path.join(os.getcwd(), "check_reduced/")  # get the path to the data input directory
 output_dir = os.path.join(os.getcwd(), "matrix_python/match_scores/")  # Sets the directory where all the saved outputs will be stored
 genes_lengths_path = os.path.join(os.getcwd(), "CDShumanGenesLengths.txt")  # path to upload the file containing each gene's ID and the correspondent gene length
+histogram_plot_path = os.path.join(os.getcwd(), "genes_histograms/")  # path to upload the file containing each gene's ID and the correspondent gene length
 
-create_dir_if_not_exist([input_dir, output_dir])
+create_dir_if_not_exist([input_dir, output_dir, histogram_plot_path])
+
+num_comparison = 5  # NOTA: numero di confronti random da eseguire per ogni coppia di file bed
 
 
 def signal_digitalisation(genes, bed_files_dicts, areReadsRandomized):
@@ -32,6 +32,8 @@ def signal_digitalisation(genes, bed_files_dicts, areReadsRandomized):
         # extract the matrices
         pd_matrix_coverage, matrix_01 = me.extract_matrices(areReadsRandomized=areReadsRandomized)
         matrix_01_list.append({'matrix': matrix_01, 'file_name': bed_file_name})
+
+        # NOTA: per velocizzare le operazioni per ora ho tolto il salvataggio
         # coverage_matrix_csv_path = os.path.join(output_dir, bed_file_name + "_matrix_coverage.csv")
         # matrix_01_csv_path = os.path.join(output_dir, bed_file_name + "_matrix_01.csv")
 
@@ -39,15 +41,6 @@ def signal_digitalisation(genes, bed_files_dicts, areReadsRandomized):
         # matrix_01.to_csv(matrix_01_csv_path, index=True)
         # pd_matrix_coverage.to_csv(coverage_matrix_csv_path, index=True)
     return matrix_01_list
-
-
-def signal_digitalisation_parallel(genes, bed_files_dict, areReadsRandomized):
-    bed_file = bed_files_dict["bed_file"]
-    bed_file_name = bed_files_dict["bed_file_name"]
-    me = MatricesExtractor(bed_file, genes)
-    # extract the matrices
-    pd_matrix_coverage, matrix_01 = me.extract_matrices(areReadsRandomized=areReadsRandomized)
-    return {'matrix': matrix_01, 'file_name': bed_file_name}
 
 
 def compute_real_match_scores(genes, bed_files_dicts):
@@ -79,20 +72,22 @@ def compute_real_match_scores(genes, bed_files_dicts):
     return gene_list, match_scores
 
 
-def compute_fake_match_scores(genes, bed_files_dicts, gene_list):
-    # matrix_01_list = Parallel(n_jobs=2, prefer="threads")(delayed(signal_digitalisation_parallel)(genes, bed_files_dict, areReadsRandomized=True) for bed_files_dict in bed_files_dicts)
-    matrix_01_list = signal_digitalisation(genes, bed_files_dicts, areReadsRandomized=True)
-
-    # lists of pair of matrices
-    pairs = [list(f) for f in combinations(matrix_01_list, 2)]
-
-    # saves match scores
+def compare_pair_n_times(bed_files_pair, genes, gene_list, n):
+    # extract a pair of bed files
     match_scores = []
-    for pair in pairs:
-        match_score, pair_names = compare_pair(pair, genes.set_index('GeneID'), gene_list)
+    for i in range(n):
+        matrix_01_pair = []
+        for bed_files_dict in bed_files_pair:
+            bed_file = bed_files_dict["bed_file"]
+            bed_file_name = bed_files_dict["bed_file_name"]
+            me = MatricesExtractor(bed_file, genes)
+            # extract the matrices
+            pd_matrix_coverage, matrix_01 = me.extract_matrices(areReadsRandomized=True)
+            matrix_01_pair.append({'matrix': matrix_01, 'file_name': bed_file_name})
+
+        match_score, pair_names = compare_pair(matrix_01_pair, genes.set_index('GeneID'), gene_list)
         pair_names = Path(pair_names[0]).stem + ":" + Path(pair_names[1]).stem
         match_scores.append({'pair_name': pair_names, 'match_score': match_score})
-
     return match_scores
 
 
@@ -102,40 +97,56 @@ def main():
     bed_files_dicts = ifm.get_bed_files()
     gene_list, match_scores = compute_real_match_scores(genes, bed_files_dicts)
 
-    # compute fake match scores
-    fake_match_scores = compute_fake_match_scores(genes, bed_files_dicts, gene_list)
-    # create a histogram for each pair and for each gene
+    # create pairs of bed files
+    bed_files_pairs = [list(f) for f in combinations(bed_files_dicts, 2)]
+
+    match_scores_list = []
+    # # randomize and digitalise each pair
+    p = multiprocessing.Pool(processes=1)
+    # NOTA: processes rappresenta il numero di processi in parallelo che eseguono i calcoli.
+    # Idealmente ce ne vorrebbe uno per ogni coppia di file bed (es. con 4 file l'ideale sarebbero 6 processi)
+    # La cosa migliore è usare il numero maggiore di processi che possono stare in memoria
+    start = time.time()
+    res = []
+    for bed_files_pair in bed_files_pairs:
+        res.append(p.apply_async(compare_pair_n_times, [bed_files_pair, genes, gene_list, num_comparison]))
+
+    p.close()
+    p.join()
+
+    for i in res:
+        match_scores_list.append(i.get())
+    end = time.time()
+    print("Complete")
+    print('total time (s)= ' + str(end - start))
+
+    print(len(match_scores_list[0]))
+
     match_scores_hist = {}
-    for fake_match_score in fake_match_scores:
-        # fake_match_score contains the scores of one pair
-        pair_name = fake_match_score['pair_name']
-        match_scores = fake_match_score['match_score']
-        gene_hist = {}
-        for gene, match_score in match_scores.items():
-            # print(f"Index : {gene}, Value : {match_score}")
-            gene_hist[gene] = [match_score]
-
-        match_scores_hist[pair_name] = gene_hist
-
-    for i in range(0, 1500):
-        start = time.process_time()
-        fake_match_scores = compute_fake_match_scores(genes, bed_files_dicts, gene_list)
+    for fake_match_scores in match_scores_list:
         for fake_match_score in fake_match_scores:
+            # fake_match_score contains the scores of one pair
             pair_name = fake_match_score['pair_name']
             match_scores = fake_match_score['match_score']
+            gene_hist = {}
             for gene, match_score in match_scores.items():
-                # print(f"Index : {gene}, Value : {match_score}")
-                match_scores_hist[pair_name][gene].append(match_score)  # = [match_score]
-        print(time.process_time() - start)
+                gene_hist[gene] = [match_score]
 
+            if pair_name in match_scores_hist:
+                for gene, match_score in match_scores.items():
+                    match_scores_hist[pair_name][gene].append(match_score)  # = [match_score]
+            else:
+                match_scores_hist[pair_name] = gene_hist
+    i = 0
     for pair_name in match_scores_hist:
         for gene in match_scores_hist[pair_name]:
-            gene_hist = match_scores_hist[pair_name][gene]
-            plt.hist(gene_hist, bins=50)
-            plt.xlabel('Score')
-            plt.ylabel('Frequency')
-            plt.title("dataset_pair:" + pair_name + " gene:" + gene)
-            plt.show()
+            gene_hist = pd.Series(match_scores_hist[pair_name][gene])
+            gene_hist.plot.hist(grid=True, bins=20, rwidth=0.9, color='#607c8e')
+            plt.savefig(histogram_plot_path+"dataset_pair:" + pair_name + " gene:" + gene + ".png")
+            plt.figure(i) #NOTA: fatto solo per evitare di sovrascrivere i plot, credo ci siano modi migliori...
+            i += 1
+
+    # NOTA: ci sarebbe da fare i confronti tra i reali e gli istogrammi creati però non ho ben capito come fare
 
 
 if __name__ == '__main__':
